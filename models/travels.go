b0e1b37e8d61fbc646fa8b4fa0e59523d2540f16
hamkasatria <muhhamkasatrianto@gmail.com>
2022-12-27 15:04:48 +0700

    fix boject id

--- models/travels.go
+++ models/travels.go
@@ -1,12 +1,8 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 // Travel struct
 type Travel struct {
-	ObjectID primitive.ObjectID `json:"id" bson:"_id`
-	Name     string             `json:"name" bson:"name"`
-	Contact  string             `json:"contact" bson:"contact"`
+	ID      string `json:"id,omitempty" bson:"_id,omitempty"`
+	Name    string `json:"name" bson:"name"`
+	Contact string `json:"contact" bson:"contact"`
 }
